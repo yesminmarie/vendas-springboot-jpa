409e8da00047067cb60cdf12e4c7fbd3eabc3428
Yesmin Lahoud <ymslahoud@gmail.com>
2022-11-07 19:37:41 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,12 @@
+<p>Esta é uma parte do projeto desenvolvido 
+no curso "Spring Boot Expert" do professor 
+Dougllas Sousa, disponível na UDEMY. </p>
+<p>Aqui foi exemplificado o uso do EntityManager com a realização 
+de um CRUD da entidade Cliente.</p>
+
+<h2>Tecnologias utilizadas</h2>
+
+- Spring Boot
+- EntityManager
+- Banco de Dados H2
+
---
+++ src/main/java/io/github/yesminmarie/VendasApplication.java
@@ -0,0 +1,55 @@
+package io.github.yesminmarie;
+
+import io.github.yesminmarie.domain.entity.Cliente;
+import io.github.yesminmarie.domain.repositorio.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
+@SpringBootApplication
+public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner init(@Autowired Clientes clientes){
+        return args -> {
+            System.out.println("Salvando clientes");
+            clientes.salvar(new Cliente("Yesmin"));
+            clientes.salvar(new Cliente("Outro cliente"));
+
+            List<Cliente> todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+
+            System.out.println("Atualizando clientes");
+            todosClientes.forEach(c -> {
+                c.setNome(c.getNome() + " atualizado.");
+                clientes.atualizar(c);
+            });
+
+            todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+
+            System.out.println("Buscando clientes");
+            clientes.buscarPorNome("cli").forEach(System.out::println);
+
+            System.out.println("Deletando clientes");
+            clientes.obterTodos().forEach(c -> {
+                clientes.deletar(c);
+            });
+
+            todosClientes = clientes.obterTodos();
+            if(todosClientes.isEmpty()){
+                System.out.println("Nenhum cliente encontrado.");
+            } else {
+                todosClientes.forEach(System.out::println);
+            }
+        };
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(VendasApplication.class, args);
+    }
+}
