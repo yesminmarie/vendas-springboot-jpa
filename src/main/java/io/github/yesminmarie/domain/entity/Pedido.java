1e05aa895367169ee9cf784397294c4017edd6ee
Yesmin Lahoud <ymslahoud@gmail.com>
2022-11-08 15:20:29 -0300

    mapeamento das classes Pedido e ItemPedido

--- src/main/java/io/github/yesminmarie/domain/entity/Pedido.java
+++ src/main/java/io/github/yesminmarie/domain/entity/Pedido.java
@@ -3,6 +3,7 @@ package io.github.yesminmarie.domain.entity;
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Table(name = "pedido")
@@ -21,6 +22,17 @@ public class Pedido {
 
     @Column(name = "total", length = 20, precision = 2)
     private BigDecimal total;
+
+    @OneToMany(mappedBy = "pedido")
+    private List<ItemPedido> itens;
+
+    public List<ItemPedido> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<ItemPedido> itens) {
+        this.itens = itens;
+    }
 
     public Integer getId() {
         return id;
