c8230408a40f48c8736da250f78eb711de6c011c
Yesmin Lahoud <ymslahoud@gmail.com>
2022-11-08 13:57:04 -0300

    usando spring data jpa

--- src/main/java/io/github/yesminmarie/VendasApplication.java
+++ src/main/java/io/github/yesminmarie/VendasApplication.java
@@ -17,35 +17,13 @@ public class VendasApplication {
     public CommandLineRunner init(@Autowired Clientes clientes){
         return args -> {
             System.out.println("Salvando clientes");
-            clientes.salvar(new Cliente("Yesmin"));
-            clientes.salvar(new Cliente("Outro cliente"));
+            clientes.save(new Cliente("Yesmin Marie"));
+            clientes.save(new Cliente("Outro cliente"));
 
-            List<Cliente> todosClientes = clientes.obterTodos();
-            todosClientes.forEach(System.out::println);
+            boolean existe = clientes.existsByNome("Yesmin");
+            System.out.println("Existe um cliente com nome Yesmin: " + existe);
 
-            System.out.println("Atualizando clientes");
-            todosClientes.forEach(c -> {
-                c.setNome(c.getNome() + " atualizado.");
-                clientes.atualizar(c);
-            });
 
-            todosClientes = clientes.obterTodos();
-            todosClientes.forEach(System.out::println);
-
-            System.out.println("Buscando clientes");
-            clientes.buscarPorNome("cli").forEach(System.out::println);
-
-            System.out.println("Deletando clientes");
-            clientes.obterTodos().forEach(c -> {
-                clientes.deletar(c);
-            });
-
-            todosClientes = clientes.obterTodos();
-            if(todosClientes.isEmpty()){
-                System.out.println("Nenhum cliente encontrado.");
-            } else {
-                todosClientes.forEach(System.out::println);
-            }
         };
     }
 
--- src/main/java/io/github/yesminmarie/domain/repositorio/Clientes.java
+++ src/main/java/io/github/yesminmarie/domain/repositorio/Clientes.java
@@ -1,63 +1,16 @@
 package io.github.yesminmarie.domain.repositorio;
 
 import io.github.yesminmarie.domain.entity.Cliente;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
-@Repository
-public class Clientes {
 
-    @Autowired
-    private EntityManager entityManager;
+public interface Clientes extends JpaRepository<Cliente, Integer> {
+    List<Cliente> findByNomeLike(String nome);
+    List<Cliente> findByNomeOrIdOrderById(String nome, Integer id);
 
-    @Transactional
-    public Cliente salvar(Cliente cliente){
-        entityManager.persist(cliente);
-        return cliente;
-    }
+    Cliente findOneByNome(String nome);
 
-    @Transactional
-    public Cliente atualizar(Cliente cliente) {
-        entityManager.merge(cliente);
-        return cliente;
-    }
-
-    @Transactional
-    public void deletar(Cliente cliente){
-        if(!entityManager.contains(cliente)){
-            cliente = entityManager.merge(cliente);
-        }
-        entityManager.remove(cliente);
-    }
-
-    @Transactional
-    public void deletar(Integer id){
-        Cliente cliente = entityManager.find(Cliente.class, id);
-        deletar(cliente);
-    }
-
-    @Transactional(readOnly = true)
-    public List<Cliente> buscarPorNome(String nome) {
-        String jpql = " select c from Cliente c where c.nome like :nome ";
-        TypedQuery<Cliente> query = entityManager.createQuery(jpql, Cliente.class);
-        query.setParameter("nome", "%" + nome + "%");
-        return query.getResultList();
-    }
-
-    @Transactional(readOnly = true)
-    public List<Cliente> obterTodos(){
-
-        return entityManager
-                .createQuery("from Cliente", Cliente.class)
-                .getResultList();
-    }
+    boolean existsByNome(String nome);
 }
