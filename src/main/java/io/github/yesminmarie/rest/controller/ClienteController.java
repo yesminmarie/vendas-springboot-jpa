e0bad8b05a05c549603f82961f2df5826b311be4
Yesmin Lahoud <ymslahoud@gmail.com>
2022-11-10 18:03:18 -0300

    filtro de Cliente

--- src/main/java/io/github/yesminmarie/rest/controller/ClienteController.java
+++ src/main/java/io/github/yesminmarie/rest/controller/ClienteController.java
@@ -2,10 +2,13 @@ package io.github.yesminmarie.rest.controller;
 
 import io.github.yesminmarie.domain.entity.Cliente;
 import io.github.yesminmarie.domain.repository.Clientes;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -58,5 +61,17 @@ public class ClienteController {
                     clientes.save(cliente);
                     return ResponseEntity.noContent().build();
                 }).orElseGet( () -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/api/clientes")
+    public ResponseEntity find(Cliente filtro){
+        ExampleMatcher matcher = ExampleMatcher
+                                    .matching()
+                                    .withIgnoreCase()
+                                    .withStringMatcher(
+                                            ExampleMatcher.StringMatcher.CONTAINING);
+        Example example = Example.of(filtro, matcher);
+        List<Cliente> lista = clientes.findAll(example);
+        return ResponseEntity.ok(lista);
     }
 }
