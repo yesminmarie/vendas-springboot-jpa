a4251369f4af89ca509cec6278ed349c9370bbe0
Yesmin Lahoud <ymslahoud@gmail.com>
2022-11-10 15:43:22 -0300

    metodo getClienteById

--- src/main/java/io/github/yesminmarie/rest/controller/ClienteController.java
+++ src/main/java/io/github/yesminmarie/rest/controller/ClienteController.java
@@ -1,18 +1,34 @@
 package io.github.yesminmarie.rest.controller;
 
+import io.github.yesminmarie.domain.entity.Cliente;
+import io.github.yesminmarie.domain.repository.Clientes;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Optional;
 
 @Controller
 public class ClienteController {
 
-    @RequestMapping(value = "/api/clientes/hello/{nome}", method = RequestMethod.GET)
+    private Clientes clientes;
+
+    public ClienteController(Clientes clientes) {
+        this.clientes = clientes;
+    }
+
+    @GetMapping("/api/clientes/{id}")
     @ResponseBody
-    public String helloCliente( @PathVariable("nome") String nomeCliente ){
-        return String.format("Hello %s ", nomeCliente);
+    public ResponseEntity<Cliente> getClienteById(@PathVariable Integer id ){
+        Optional<Cliente> cliente = clientes.findById(id);
+
+        if(cliente.isPresent()){
+            return ResponseEntity.ok(cliente.get());
+        }
+
+        return ResponseEntity.notFound().build();
     }
 
 }
