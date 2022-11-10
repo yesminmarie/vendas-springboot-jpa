770ec74021e0ed203a3304d5b1d52c05d405681b
Yesmin Lahoud <ymslahoud@gmail.com>
2022-11-10 16:39:12 -0300

    post e delete Cliente

--- src/main/java/io/github/yesminmarie/VendasApplication.java
+++ src/main/java/io/github/yesminmarie/VendasApplication.java
@@ -1,23 +1,11 @@
 package io.github.yesminmarie;
 
-import io.github.yesminmarie.domain.entity.Cliente;
-import io.github.yesminmarie.domain.repository.Clientes;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VendasApplication {
 
-    @Bean
-    public CommandLineRunner commandLineRunner(@Autowired Clientes clientes){
-        return args -> {
-            Cliente c = new Cliente(null, "Fulano");
-            clientes.save(c);
-        };
-    }
     public static void main(String[] args) {
 
         SpringApplication.run(VendasApplication.class, args);
--- src/main/java/io/github/yesminmarie/rest/controller/ClienteController.java
+++ src/main/java/io/github/yesminmarie/rest/controller/ClienteController.java
@@ -4,9 +4,7 @@ import io.github.yesminmarie.domain.entity.Cliente;
 import io.github.yesminmarie.domain.repository.Clientes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -23,12 +21,29 @@ public class ClienteController {
     @ResponseBody
     public ResponseEntity<Cliente> getClienteById(@PathVariable Integer id ){
         Optional<Cliente> cliente = clientes.findById(id);
-
         if(cliente.isPresent()){
             return ResponseEntity.ok(cliente.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping("/api/clientes")
+    @ResponseBody
+    public ResponseEntity save(@RequestBody Cliente cliente){
+        Cliente clienteSalvo = clientes.save(cliente);
+        return ResponseEntity.ok(clienteSalvo);
+    }
+
+    @DeleteMapping("/api/clientes/{id}")
+    @ResponseBody
+    public ResponseEntity delete(@PathVariable Integer id){
+        Optional<Cliente> cliente = clientes.findById(id);
+
+        if(cliente.isPresent()){
+            clientes.delete(cliente.get());
+            return ResponseEntity.noContent().build();
         }
 
         return ResponseEntity.notFound().build();
     }
-
 }
