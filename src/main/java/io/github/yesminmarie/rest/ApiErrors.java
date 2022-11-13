818e0153f1ccda99a11822f7104227c6ffd8072a
Yesmin Lahoud <ymslahoud@gmail.com>
2022-11-13 15:52:58 -0300

    utilizando ControllerAdvice e ExceptionHandlers para tratar erros

---
+++ src/main/java/io/github/yesminmarie/rest/ApiErrors.java
@@ -0,0 +1,16 @@
+package io.github.yesminmarie.rest;
+
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ApiErrors {
+
+    @Getter
+    private List<String> errors;
+
+    public ApiErrors(String mensagemErro){
+        this.errors = Arrays.asList(mensagemErro);
+    }
+}
---
+++ src/main/java/io/github/yesminmarie/rest/controller/ApplicationControllerAdvice.java
@@ -0,0 +1,19 @@
+package io.github.yesminmarie.rest.controller;
+
+import io.github.yesminmarie.exception.RegraNegocioException;
+import io.github.yesminmarie.rest.ApiErrors;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(RegraNegocioException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleRegraNegocioException(RegraNegocioException ex){
+        String mensagemErro = ex.getMessage();
+        return new ApiErrors(mensagemErro);
+    }
+}
