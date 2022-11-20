daf1657bffd0227a51a810d947a46f01d77339f1
Yesmin Lahoud <ymslahoud@gmail.com>
2022-11-20 15:18:16 -0300

    Validando a entidade Cliente

--- src/main/java/io/github/yesminmarie/rest/controller/ApplicationControllerAdvice.java
+++ src/main/java/io/github/yesminmarie/rest/controller/ApplicationControllerAdvice.java
@@ -4,9 +4,13 @@ import io.github.yesminmarie.exception.PedidoNaoEncontradoException;
 import io.github.yesminmarie.exception.RegraNegocioException;
 import io.github.yesminmarie.rest.ApiErrors;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ApplicationControllerAdvice {
@@ -22,5 +26,15 @@ public class ApplicationControllerAdvice {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ApiErrors handlePedidoNaoEncontradoException(PedidoNaoEncontradoException ex){
         return new ApiErrors(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+        List<String> errors = ex.getBindingResult().getAllErrors()
+                .stream()
+                .map(erro -> erro.getDefaultMessage())
+                .collect(Collectors.toList());
+        return new ApiErrors(errors);
     }
 }
