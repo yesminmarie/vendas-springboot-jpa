185d562ef03d5679002263db56a4cf8fc99286b7
Yesmin Lahoud <ymslahoud@gmail.com>
2022-11-30 20:58:23 -0300

    Criação de arquivo properties para dev

--- src/main/java/io/github/yesminmarie/config/SecurityConfig.java
+++ src/main/java/io/github/yesminmarie/config/SecurityConfig.java
@@ -72,6 +72,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 "/swagger-resources/**",
                 "/configuration/security",
                 "/swagger-ui.html",
-                "/webjars/**");
+                "/webjars/**",
+                "/h2-console/**");
     }
 }
